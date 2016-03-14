0122c3106da74d5481a50d3fafa0127b37320765
tinitre <pedde77@gmail.com>
2016-03-14 12:18:24 +0100

    midified Ranking.java

--- Ranking.java
+++ Ranking.java
@@ -1,76 +1,74 @@
 import java.util.Scanner;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ranking{
 
 	private final int MAX_PEOPLE_LIMIT=5;
-	private String[] name;
-	private int[] record;
 	private int last;
-	
+	public ArrayList<Record> records;	//records are stored here and sorted
+
 	Ranking(){
-		name=new String[MAX_PEOPLE_LIMIT];
-		record=new int[MAX_PEOPLE_LIMIT];
-		
-		last=0;
+		records = new ArrayList<Record>(); //initiates the ranking list
+		last=0; //initiates last checker
 	}
 
 
 	public void recordName(int result) {
+		last++;
+		if(last > MAX_PEOPLE_LIMIT) { //will throw out your score if you don't beat the 5th score
+		try {
+			if(result <= records.get(MAX_PEOPLE_LIMIT-1).score && records.get(MAX_PEOPLE_LIMIT-1) != null) {
+				System.out.println("Sorry, but your score was too low to enter the hiscores");
+				show();
+				return; //returns as there is no need to record the score
+			}
+		}
+		catch(IndexOutOfBoundsException e) {}
+		}
 		System.out.print("\n Please enter your name -");
 		Scanner in=new Scanner(System.in);
 		String newName=in.nextLine();
-		if((last==MAX_PEOPLE_LIMIT)&&record[MAX_PEOPLE_LIMIT-1]>result){
-			System.out.println("\nSorry you cannot enter top "+(MAX_PEOPLE_LIMIT)+" players");
-			return;
-		}
-		else if(last==MAX_PEOPLE_LIMIT){
-			name[MAX_PEOPLE_LIMIT-1]=newName;{
-				record[MAX_PEOPLE_LIMIT-1]=result;
-			}
-		}
-		else{
-			name[last]=newName;{
-				record[last]=result;{
-					last++;
-				}
-			}
-		}
-		
+		records.add(new Record(newName, result)); //records the player
 		sort();
 		show();
 	}
 
 
-	public void show() {
-		if(last==0){
-			System.out.println("Still no results");
-			return;
-		}
-		System.out.println("N Name\t\tresult");
-		for(int i=0;i<last;i++){
-			System.out.println((i+1)+" "+name[i]+"\t"+record[i]);
+	public void show() { //iterates the records list, using toString
+		int i = 1;
+		for(Record r : records) {
+			System.out.println(i + "    " + r);
+			if(i == MAX_PEOPLE_LIMIT) break; //breaks the print when the 5 best are shown
+			i++;
 		}
 	}
-	
-	
+
+	public static Comparator<Record> SortByScore = new Comparator<Record>() {
+		//this statement lets us change the compare method to what we need it for
+	public int compare(Record r1, Record r2) {
+	   int score1 = r1.score;
+	   int score2 = r2.score;
+	   return score2-score1; //sorts the list descending
+   }};
+
 	private void sort(){
-		if(last<2) return;
-		boolean unsorted=true;
-		while(unsorted){
-			unsorted=false;
-			for(int i=0;i<(last-1);i++){
-				if(record[i+1]>record[i]){
-					int swapR=record[i];{
-						record[i]=record[i+1];{
-							record[i+1]=swapR;
-							String swapN=name[i];
-							name[i]=name[i+1];
-							name[i+1]=swapN;
-							unsorted=true;
-						}
-					}
-				}
-			}
-		}
+		Collections.sort(records, Ranking.SortByScore); //way less complex method for sorting
+		if(records.size() > MAX_PEOPLE_LIMIT) records.remove(MAX_PEOPLE_LIMIT); //removes whoever has fallen out of the hiscore
+		System.out.println(records.size());
+	}
+}
+
+class Record { //class for storing records, as the previous methods were messy
+	public String name;
+	public int score;
+
+	Record(String n, int s) {
+		name = n;
+		score = s;
+	}
+	public String toString() {
+		return "name: " + name + ", score: " + score;
 	}
 }
