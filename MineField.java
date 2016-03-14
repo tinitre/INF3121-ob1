33f2b41d4b71b6a49d9631d85bb0ca3c717081e5
Kai Arne Schröder Myklebust <applepai123@users.noreply.github.com>
2016-03-14 12:35:08 +0100

    Updated java files

--- MineField.java
+++ MineField.java
@@ -4,36 +4,39 @@ class MineField{
 
 	private boolean[][] mines,visible;
 	private boolean boom;
-	private final int rowMax = 10;
-	private final int colMax = 20;
-
+	private final int rowMax = 5;
+	private final int colMax = 10;
+	
+	//initialize minefield, and ads 15 mines in the minefield
 	MineField(){
-
+		
 		mines=new boolean[rowMax][colMax];
 		visible=new boolean[rowMax][colMax];
 		boom=false;
-
+		
 		for(int row=0;row<rowMax;row++){
 			for(int col=0;col<colMax;col++){
 				mines[row][col]=false;
 				visible[row][col]=false;
 			}
 		}
-
+		
 		int counter2=15;
 		int randomRow,randomCol;
 		Random RGenerator=new Random();
-
+		
 		while(counter2>0){
-
+			
 			randomRow=Math.abs(RGenerator.nextInt()%rowMax);
 			randomCol=Math.abs(RGenerator.nextInt()%colMax);
-
+			
 			if(trymove(randomRow,randomCol)){
 				counter2--;
 			}
 		}
-	}
+	}	
+	//if this row and coloum has a mine it return false 
+	//and if there is no mine it sets a mine there and returns true
 	private boolean trymove(int randomRow, int randomCol) {
 		if(mines[randomRow][randomCol]){
 			return false;
@@ -43,6 +46,9 @@ class MineField{
 			return true;
 		}
 	}
+	//boom makes all rows and coloums visible, 
+	//sets boom to true to show that a mine has been hit
+	//and shows the field to the player
 	private void boom() {
 		for(int row=0;row<rowMax;row++){
 			for(int col=0;col<colMax;col++){
@@ -52,14 +58,12 @@ class MineField{
 			}
 		}
 		boom=true;
-		show();
-
-
+		show();	
 	}
-
-
+	//returns * if there is a mine, - if a mine has been hit, and a number for showing how many mines are adjecent to this field
 	private char drawChar(int row, int col) {
 		int count=0;
+		//counts how many bombs are adjacent to this field
 		if(visible[row][col]){
 			if(mines[row][col]) return '*';
 			for(int irow=row-1;irow<=row+1;irow++){
@@ -75,8 +79,6 @@ class MineField{
 				return '-';
 			}
 			{
-
-
 				return '?';
 			}
 		}
@@ -90,24 +92,22 @@ class MineField{
 		case 6:return '6';
 		case 7:return '7';
 		case 8:return '8';
-
-
+		
+		
 		default:return 'X';
 		}
 	}
+	//returns true if a mine has been hit else it returns false
 	public boolean getBoom(){
-
+		
 		return boom;
 	}
-
-
+	//checks if the input is a legal input
 	public boolean legalMoveString(String input) {
 		String[] separated=input.split(" ");
 		int row;
 		int col;
 		try{
-
-
 			row=Integer.parseInt(separated[0]);
 			col=Integer.parseInt(separated[1]);
 			if(row<0||col<0||row>=rowMax||col>=colMax){
@@ -118,20 +118,16 @@ class MineField{
 			System.out.println("\nInvalid Input!");
 			return false;
 		}
-
+		//checks if the input is legal
 		if(legalMoveValue(row,col)){
 			return true;
-
-
 		}
 		else{
 			return false;
 		}
 	}
-
-
+	//checks if you already steped in the area or if there is a mine in this area.
 	private boolean legalMoveValue(int row, int col) {
-
 		if(visible[row][col]){
 			System.out.println("You stepped in allready revealed area!");
 			return false;
@@ -139,32 +135,27 @@ class MineField{
 		else{
 			visible[row][col]=true;
 		}
-
+		
 		if(mines[row][col]){
-			//System.out.println("This area has a bomb in it!");
 			boom();
 			return false;
 		}
-
+		
 		return true;
 	}
+	//prints out the minefield
 	public void show() {
-		System.out.print("\n    ");
-		for(int i = 0; i <colMax; i++) {
-			System.out.print(" " + i);
-		}
-		System.out.print("\n");
-		//System.out.println("\n    0 1 2 3 4 5 6 7 8 9 ");
+		System.out.println("\n    0 1 2 3 4 5 6 7 8 9 ");
 		System.out.println("   ---------------------");
 		for(int row=0;row<rowMax;row++){
 			System.out.print(row+" |");
 			for(int col=0;col<colMax;col++){
 				System.out.print(" "+drawChar(row,col));
-
+				
 			}
 			System.out.println(" |");
 		}
 		System.out.println("   ---------------------");
 	}
-
+	
 }
--- Minesweeper.java
+++ Minesweeper.java
@@ -4,39 +4,43 @@ public class Minesweeper {
 
 	private static MineField field;
 	private static Ranking rank;	
+	
+	//initializes the ranking, prints out how to play and starts the game
 	public static void main(String[] args) {
-		rank=new Ranking();
-		mainMessage()
-		
-		;
+		rank = new Ranking();
+		mainMessage();
 		while(gameCountinue());
 		System.out.println("\nThank you for playing :) Have a nice day!");
 	}	
+	
+	//plays game, checks input for move or commands
 	private static boolean gameCountinue() {
 		field = new MineField();
 		int result = 0;
 		while (true) {
-
 			field.show();
 			System.out.print("\nPlease enter your move(row col): ");
 			Scanner in = new Scanner(System.in);
 			String input = in.nextLine();
 
+			//if input is top, it shows the ranking and continues game
 			if (input.equals("top")) {
 				rank.show();
 				continue;
-
-
 			}
-			if (input.equals("restart")) {
+			//if input is restart, it ads the current result to the ranking and starts a new game
+			else if (input.equals("restart")) {
 				rank.recordName(result);
 				return true;
 			}
-			if (input.equals("exit")) {
+			//if input is exit, it ads the current result to the ranking and ends the game
+			else if (input.equals("exit")) {
 				rank.recordName(result);
 				return false;
 			}
-			if (field.legalMoveString(input)) {
+			//if the input is a legal move it ads 1 to the number of turns
+			//and if the number of turns is 35, the field is cleared and it prints you won
+			else if (field.legalMoveString(input)) {
 				result++;
 				if (result == 35) {
 					System.out.println("Congratulations you WON the game!");
@@ -49,31 +53,15 @@ public class Minesweeper {
 				}
 				continue;
 			}
-//			if(input.equals("exit")){
-//				rank.recordName(result);
-//				return false;
-//			}
-//			if(field.legalMoveString(input)){
-//				result++;
-//				if(result==35){
-//					System.out.println("Congratulations you WON the game!");
-//					rank.recordName(result);
-//					return true;
-//				}
-//				continue;
-//			}
+			//if the input you chose has a mine, it prints out boooom and records your name with the current turn
 			else if (field.getBoom()) {
 				System.out.println("\nBooooooooooooooooooooooooooooom!You stepped on a mine!You survived " + result + " turns");
 				rank.recordName(result);
 				return true;
 			}
-
 		}
-
-
-		}
-
-	
+	}
+	//prints the instructions to play this game
 	private static void mainMessage(){
 		System.out.println("Welcome to Minesweeper!");
 		System.out.println("To play just input some coordinates and try not to step ont mine :)");
